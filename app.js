90fc139666cd92d25c826534f58e61b0986393d8
Alexander Dymov <75511267+drpe90@users.noreply.github.com>
2023-08-18 17:33:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// API url: http://www.boredapi.com/api/activity/
+
+const url = "http://www.boredapi.com/api/activity/";
+
+const btn = document.querySelector(".button");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  whatToDo();
+});
+
+const whatToDo = async () => {
+`⏳ Just a sec...`
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    result.textContent = data.activity;
+  } catch (err) {
+    result.textContent = `You are beyond help. 💩`;
+  }
+};
+
+whatToDo();
